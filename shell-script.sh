105268f993210ad626e250e4f9fc513019f62583
ops-sl <sudhanshu.nov9@gmail.com>
2025-02-23 21:54:10 +0530

    Create shell-script.sh

---
+++ shell-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run the ls -lrta command
+ls -lrta
+
+# Create a file named lavi
+touch lavi
+
+# Optional: Print a message indicating the file has been created
+echo "File 'lavi' has been created."
